b97286d1e008f275369793a96ffa32275c3989a9
Harshita Nidhi <harshitanidhi17@gmail.com>
2023-05-26 00:06:25 +0530

    first commit

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1e534ed02e1c2eab0c7157ebb5f1d33b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
